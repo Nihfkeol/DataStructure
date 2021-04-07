ec8af80b7c34366a3b2febb7588731f2541f2637
Nihfkeol <nihfkeol@163.com>
2021-04-07 18:39:59 +0800

    one

---
+++ Stack.c
@@ -0,0 +1,12 @@
+//
+// Created by Nihfkeol on 2021/4/7.
+//
+
+#include "malloc.h"
+
+typedef int DateType;
+
+typedef struct stacknode{
+    DateType date;
+    struct stacknode *next;
+} LinkStack;
